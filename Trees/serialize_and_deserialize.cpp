8f9541e3a4eaa4a54b11b8ced0a3d17b2857186d
AreYouKhush <71877944+AreYouKhush@users.noreply.github.com>
2024-08-23 20:08:07 +0530

    update: day 40

---
+++ Trees/serialize_and_deserialize.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        ostringstream temp;
+        
+        ToString(root, temp);
+        
+        return temp.str();
+    }
+
+    TreeNode* deserialize(string data) {
+        istringstream temp(data);
+        
+        return ToStructure(temp);
+    }
+    
+private:
+    
+    void ToString(TreeNode* root, ostringstream& temp) {
+        if (root == NULL) {
+            temp << "N ";
+            return;
+        }
+        
+        temp << root->val << " ";
+        
+        ToString(root->left, temp);
+        ToString(root->right, temp);
+    }
+    
+    TreeNode* ToStructure(istringstream& temp) {
+        string value = "";
+        temp >> value;
+        
+        if (value == "N") {
+            return NULL;
+        }
+        
+        TreeNode* root = new TreeNode(stoi(value));
+        
+        root->left = ToStructure(temp);
+        root->right = ToStructure(temp);
+        
+        return root;
+    }
+    
+};
+
+int main() {
+    return 0;
+}
